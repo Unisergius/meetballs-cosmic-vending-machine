f3e1cceb67b3905f59fb433bdffec96516b45aeb
shenriquesdev <Unisergius@gmail.com>
2025-08-06 19:26:38 +0100

    feat: removed unused

--- src/routes/vendingmachine.tsx
+++ src/routes/vendingmachine.tsx
@@ -1,5 +1,4 @@
 import { createFileRoute } from '@tanstack/react-router'
-import Header from './-components/Header'
 import Catalog from './-components/Catalog'
 import Sidebar from './-components/Sidebar'
 
